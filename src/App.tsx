d90241c05d26343f57b44090b68698053f810323
Mhayk Whandson <hi@mhayk.com>
2021-09-20 22:20:22 +0100

    Provider has been imported with our store.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+
+import store from './store';
+
+import Catalog from './components/Catalog';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
   );
 }
 
