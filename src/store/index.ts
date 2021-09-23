486765c09d2dade23e6ed30dcb999437e80ab901
Mhayk Whandson <hi@mhayk.com>
2021-09-23 17:27:32 +0100

    root reducer has been imported in the store.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'redux'
+import rootReducer from './modules/rootReducer'
 
 // initial state of the store
-const store = createStore(() => [])
+const store = createStore(rootReducer)
 
 export default store
