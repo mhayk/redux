8e286d0df0b2246546a5fea2a868a48189694f2f
Mhayk Whandson <hi@mhayk.com>
2021-09-24 19:40:17 +0100

    IState interface has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
 import { createStore } from 'redux'
+import { ICartState } from './modules/cart/types'
 import rootReducer from './modules/rootReducer'
+
+export interface IState {
+    cart: ICartState;
+}
 
 // initial state of the store
 const store = createStore(rootReducer)
